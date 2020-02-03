8e49e464aee7df522f5f9aa3af42e490c5a8b705
Arshad <arshad.researcher@gmail.com>
2020-02-03 15:47:37 +0100

    Updates

---
+++ precache-manifest.9bb6056a52753a407fec3e146ab0ec03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a8b66b0242086d548dea35145d521b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b212fef8bfe4b951089b",
+    "url": "/monsters-rolodex/static/css/main.ea9dc372.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b212fef8bfe4b951089b",
+    "url": "/monsters-rolodex/static/js/main.805cbf1d.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
